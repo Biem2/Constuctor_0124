e410230071e7f3f0da0be4ad566fca7f9628e8d6
Biem2 <abiem.muhammad.ft23@mail.umy.ac.id>
2025-05-13 19:15:45 +0700

    membuat int main dari constructor over loading

--- ConstructorOverLoading.cpp
+++ ConstructorOverLoading.cpp
@@ -13,3 +13,33 @@ mahasiswa(string);
 mahasiswa(int iNim, string iNama);
 void cetak();
 };
+
+mahasiswa :: mahasiswa(int iNim){
+    nim = iNim;
+}
+
+mahasiswa :: mahasiswa(string iNama){
+    nama = iNama;
+}
+
+mahasiswa :: mahasiswa(int iNim, string iNama){
+    nim = iNim;
+    nama = iNama;
+}
+
+void mahasiswa::cetak(){
+    cout << endl << "Nim = " <<nim << endl;
+    cout << "Nama = " <<nama << endl;
+}
+
+int main(){
+    mahasiswa mhs1(102030);
+    mahasiswa mhs2("Andi");
+    mahasiswa mhs3(010203, "santi");
+
+    cout << "mahasiswa 1:"; mhs1.cetak();
+    cout << "mahasiswa 2:"; mhs2.cetak();
+    cout << "mahasiswa 3:"; mhs3.cetak();
+
+    return 0;
+}
