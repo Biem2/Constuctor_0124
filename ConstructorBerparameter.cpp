03a0932c6b51ce83cc7f6b2f19715208d6c2c7f5
Biem2 <abiem.muhammad.ft23@mail.umy.ac.id>
2025-05-13 19:00:37 +0700

    membuat int main constructor berparameter

--- ConstructorBerparameter.cpp
+++ ConstructorBerparameter.cpp
@@ -11,5 +11,9 @@ mahasiswa::mahasiswa(int nim, string nama) {
     cout << "Constructor Dengan Parameter Terpanggil" << endl;
     cout << "NIM : " << nim << endl;
     cout << "Nama : " << nama << endl;
+}
 
+int main() {
+    mahasiswa mhs(12345, "Pascal");
+    return 0;
 }
